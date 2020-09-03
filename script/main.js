eed0a6aef5358044e103695bf7953e1a831a7e2b
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-09-03 08:54:12 -0500

    animacion del menu mobile

--- script/main.js
+++ script/main.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector('.burguer-btn')
 const mobile = document.querySelector('.mobile')
+const navMobile = document.querySelector('.navMobile')
 let show = false
 
 menu.addEventListener('click', () => {
   if (show == false) {
     mobile.style.display = 'flex'
+    navMobile.classList.add('slide-in-top')
     show = true
   } else {
     mobile.style.display = 'none'
